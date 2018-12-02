31170937250317d1508711145fb71b1c658d682b
nobodino <dib.nobodi@free.fr>
2018-12-02 11:23:07 +0100

    updated list_generator, sfs-boostrap and sfsbuild1 to build snownews

--- sfsbuild1.sh
+++ sfsbuild1.sh
@@ -2397,6 +2397,11 @@ while (( LINE < $FILELEN )); do
 					esac
 					continue ;;
 
+				snownews )
+					source /root/.profile
+					build $SRCDIR $PACKNAME
+					[ $? != 0 ] && exit 1 ;;
+
 				subversion )
 					case $LISTFILE in
 						build3_s.list )
