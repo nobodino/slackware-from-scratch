16ecfbd623fd3ed6f57c74980c2e1d854d7cf08d
nobodino <dib.nobodi@free.fr>
2019-03-28 15:40:06 +0100

    updated sfsbuild1.sh

--- sfsbuild1.sh
+++ sfsbuild1.sh
@@ -2082,7 +2082,7 @@ while (( LINE < $FILELEN )); do
 					continue ;;
 
 				dhcpcd_up )
-					dhcpcd -t 10 eth0 && echo
+					dhcpcd -t 15 -L eth0 || dhcpcd -t 15 -L wlan0 && echo
 					[ $? != 0 ] && exit 1 ;;
 
 				end1 )
