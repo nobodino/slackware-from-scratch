00b5c81e5b165327f3c0971e59da58ace3750291
nobodino <dib.nobodi@free.fr>
2019-05-31 10:22:03 +0200

    updated sfs-tools-current.sh for gettext

--- sfs-tools-current.sh
+++ sfs-tools-current.sh
@@ -657,17 +657,12 @@ gettext_build () {
 #*****************************
     tar xvf gettext-$GETTVER.tar.xz && cd gettext-$GETTVER
 
-    cd gettext-tools
-    EMACS="no" ./configure --prefix=/tools --disable-shared || exit 1
+    ./configure --disable-shared || exit 1
 
-    make -C gnulib-lib || exit 1
-    make -C intl pluralx.c || exit 1
-    make -C src msgfmt || exit 1
-    make -C src msgmerge || exit 1
-    make -C src xgettext || exit 1
+    make || exit 1
 
-    cp -v src/{msgfmt,msgmerge,xgettext} /tools/bin || exit 1
-    cd ../..
+    cp -v gettext-tools/src/{msgfmt,msgmerge,xgettext} /tools/bin || exit 1
+    cd ..
     rm -rf gettext-$GETTVER
 	echo gettext-$GETTVER >> $SFS/tools/etc/tools_version
 }
