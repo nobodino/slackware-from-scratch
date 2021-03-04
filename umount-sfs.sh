eaea904c18847558d8051dabe49a2754af918247
nobodino <dib.nobodi@free.fr>
2021-03-04 20:08:26 +0100

    shellcheck on umount-sfs.sh

--- umount-sfs.sh
+++ umount-sfs.sh
@@ -1,7 +1,7 @@
-#/bin/sh
-umount -v $SFS/dev/pts
-umount -v $SFS/dev
-umount -v $SFS/run
-umount -v $SFS/proc
-umount -v $SFS/sys
+#!/bin/sh
+umount -v "$SFS"/dev/pts
+umount -v "$SFS"/dev
+umount -v "$SFS"/run
+umount -v "$SFS"/proc
+umount -v "$SFS"/sys
 
