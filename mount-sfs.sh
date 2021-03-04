040a38a9e090df7554b062f89414a1c71da5bdb7
nobodino <dib.nobodi@free.fr>
2021-03-04 20:05:04 +0100

    shellcheck on mount-sfs.sh

--- mount-sfs.sh
+++ mount-sfs.sh
@@ -1,9 +1,9 @@
-#/bin/sh
-mount -v --bind /dev $SFS/dev
-mount -vt devpts devpts $SFS/dev/pts -o gid=5,mode=620
-mount -vt proc proc $SFS/proc
-mount -vt sysfs sysfs $SFS/sys
-mount -vt tmpfs tmpfs $SFS/run
-if [ -h $SFS/dev/shm ]; then
-  mkdir -pv $SFS/$(readlink $SFS/dev/shm)
+#!/bin/bash
+mount -v --bind /dev "$SFS"/dev
+mount -vt devpts devpts "$SFS"/dev/pts -o gid=5,mode=620
+mount -vt proc proc "$SFS"/proc
+mount -vt sysfs sysfs "$SFS"/sys
+mount -vt tmpfs tmpfs "$SFS"/run
+if [ -h "$SFS"/dev/shm ]; then
+  mkdir -pv "$SFS"/"$(readlink "$SFS"/dev/shm)"
 fi
