d1a8ab9fbfc7c9ff601ce70082f30c115e1762ea
nobodino <dib.nobodi@free.fr>
2021-11-06 05:27:12 +0100

    updated sfs-bootstrap

--- backup/shellchecked/mount-sfs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mount -v --bind /dev "$SFS"/dev
-mount -vt devpts devpts "$SFS"/dev/pts -o gid=5,mode=620
-mount -vt proc proc "$SFS"/proc
-mount -vt sysfs sysfs "$SFS"/sys
-mount -vt tmpfs tmpfs "$SFS"/run
-if [ -h "$SFS"/dev/shm ]; then
-  mkdir -pv "$SFS"/"$(readlink "$SFS"/dev/shm)"
-fi
--- backup/shellchecked/umount-sfs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-umount -v "$SFS"/dev/pts
-umount -v "$SFS"/dev
-umount -v "$SFS"/run
-umount -v "$SFS"/proc
-umount -v "$SFS"/sys
-
