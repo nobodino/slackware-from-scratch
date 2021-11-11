5e58782bd1d338431bae5ef1717576f9ee357ce8
nobodino <dib.nobodi@free.fr>
2021-11-11 13:33:26 +0100

    updated

---
+++ musl/link-musl.sh
@@ -0,0 +1,25 @@
+#***********************************************************
+#!/bin/bash
+#***********************************************************
+# Some programs use hard-wired paths to programs which do not
+# exist yet. In order to satisfy these programs, create a
+# number of symbolic links which will be replaced by real
+# files throughout the course of this chapter after the
+# software has been installed. And create some directories
+# to install the first slackware programs.
+# We added a link to /tools/bin/du to avoid 'noise' during
+# pkgtools building, which is not in LFS of course.
+#
+# Note: Much of this script is copied from the LFS manual.
+#       Copyright © 1999-2021 Gerard Beekmans and may be
+#       copied under the MIT License.
+#****************************************************************
+mkdir -pv /usr/lib && mkdir -v /bin && mkdir -pv /usr/include
+mkdir -pv /usr/src && mkdir -pv /usr/bin
+ln -sv /tools/bin/{bash,cat,dd,du,echo,ln,pwd,rm,stty} /bin
+ln -sv /tools/bin/{install,perl} /usr/bin
+ln -sv /tools/lib/libgcc_s.so{,.1} /usr/lib
+ln -sv /tools/lib/libstdc++.{a,so{,.6}} /usr/lib
+ln -sv bash /bin/sh
+ln -sv /proc/self/mounts /etc/mtab
+echo
